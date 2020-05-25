95776e95f239f178942b50b668ef43ff66a025a0
batCCAA44 <64896757+batCCAA44@users.noreply.github.com>
2020-05-25 15:13:11 +0200

    Update questions.cpp
    Ajout de DemandeDeVicci

--- VICCI/questions.cpp
+++ VICCI/questions.cpp
@@ -1,5 +1,8 @@
 #include "questions.h"
 #include <iostream>
+#include <stdlib.h>
+
+using namespace std;
 
 Questions::Questions()
 {
@@ -13,7 +16,28 @@ void Questions::afficher()
 
 void Questions::DemandeDeVicci()
 {
+    int i;
+    //for (i = 0; i < 3; i++)
+    if (i < 3)
+    {
+     if(i > 3)
+     {
+         i = 0;
+     }
 
+    switch (i) {
+    case 1:
+        cout<<"Bonjour ! Que puis-je pour vous"<<ends;
+        break;
+    case 2:
+        cout<<"Bonjour, en quoi puis-je vous etres utile ?"<<ends;
+        break;
+    case 3:
+        cout<<"Bonjour, quelle est votre requete ?"<<ends;
+        break;
+    }
+
+}
 }
 
 void Questions::InitilialiserQuestions()
