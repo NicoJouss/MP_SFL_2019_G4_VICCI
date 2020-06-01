1b86ffd35a0b98a4aaea553bc119303dbeb88a17
batCCAA44 <64896757+batCCAA44@users.noreply.github.com>
2020-06-02 00:16:26 +0200

    Update interpretation.cpp
    Ne compile pas pour le moment. J'essaye des choses.

--- VICCI/interpretation.cpp
+++ VICCI/interpretation.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 enum mots
 {
-    mot1 = temps;
-    mot2 = aujourdhui;
-    mot3 = demain;
-    mot4 = apres-demain;
-    mot5 = heure;
-    mot6 = musique
+    mot1,   //temps
+    mot2,   //aujourdhui,
+    mot3,   //demain,
+    mot4,   //musique,
+    mot5    //heure
+};
+enum genre //genre de musique
+{
+    pop,
+    electro,
+    rap,
+    rock,
 };
 
 Interpretation::Interpretation()
@@ -23,26 +29,27 @@ void Interpretation::afficher()
 {
 //    if(strstr(texteEntré, "salut"))
 //    {
-//      // "salut" a été entré
+//      // "salut" a été entré                                       (cela est un test)
 //    }
 }
 
 int Interpretation::VerificationDePhrase()
 {
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 
-{
-    long ascii = 0;
-    char lettre = 0;
+//{
+//    long ascii = 0;
+//    char lettre = 0;                                                (cela ne fonctionne pas)
 
-    lettre = 'a';
-    ascii = lettre;
-    printf("Lettre: %c\nCode ASCII: %ld\n", lettre, ascii);
-    printf("%c", ascii);
+//    lettre = 'a';
+//    ascii = lettre;
+//    printf("Lettre: %c\nCode ASCII: %ld\n", lettre, ascii);
+//    printf("%c", ascii);
 
-    return 0;
-}
+//    return 0;
+//}
+
 }
 
 void LectureDePhrase(Interpretation * p)
@@ -55,16 +62,17 @@ void LectureDePhrase(Interpretation * p)
 
         for(int i = 0; i<30; i ++)
         {
+
             long ascii = 0;
             std::string phrase1 = "Quelle temps fera-t-il demain ?";
             std::string phrase2 = "Quelle heure est-il ?";
-            //std::string mot = "temps"; //faire enum de mots pour la meteo
+            mot1 = "temps";
             std::cout<<phrase1<<"\n"<<endl;
             std::cin>>phrase1;
 
             if ( phrase1.find(mot1) != -1)
             {
-                switch (phrase1) {
+                switch (phrase1.find(mot1) != -1) {                          //en developpement
                 phrase1.find(mot1) != -1;
                     cout<<"_ "<<ends;
                     break;
